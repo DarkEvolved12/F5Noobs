4dab4ea779b88dd5d12a112591e18aa2176a5b1f
lorcrim <criado.manzaneque@gmail.com>
2020-07-14 15:31:50 +0200

    dropdown filtros

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+
+    document.getElementById("testDropdownBoton").addEventListener('click', FuncionTest);
+
+  function FuncionTest(){
+    document.getElementById("testDropdownDesplegable").style.display = 'block';
+  }
